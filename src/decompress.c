a20530ab470e1903a7f9dab0e4c0d3c05f3905b1
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2020-06-27 14:24:41 +0300

    refaktoroitu

--- src/decompress.c
+++ src/decompress.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 #include "util.h"
 
-void decompress(data file) {
+// Jäsentää little endian -muodossa koodatun 16-bittisen luvun
+unsigned short parse_short(unsigned char *buffer) {
+	return buffer[0] | (buffer[1] << 8);
+}
+
+// Jäsentää little endian -muodossa koodatun 32-bittisen luvun
+unsigned long parse_long(unsigned char *buffer) {
+	return buffer[0] |
+		(buffer[1] << 8) |
+		(buffer[2] << 16) |
+		(buffer[3] << 24);
+}
+
+// Purkaa parametrina annetun datan ja tulostaa sen
+void decompress(data compressed_data) {
 	// Pakatun datan alussa on pakkaamaton koko
-	unsigned long uncompressed_length =
-		file.buffer[0] |
-		(file.buffer[1] << 8) |
-		(file.buffer[2] << 16) |
-		(file.buffer[3] << 24);
+	unsigned long uncompressed_length = parse_long(compressed_data.buffer);
 	fprintf(stderr, "Uncompressed size: %ld\n", uncompressed_length);
 
 	// Varataan tilapäinen puskuri pakkaamattomalle datalle
 	unsigned char *buffer = malloc(uncompressed_length);
 
 	// pointer-muuttujassa on puskurin koko eli seuraava tyhjä paikka
-	long pointer = 0;
-	for (long i = 4; i < file.length; i++) {
+	unsigned long pointer = 0;
+	for (long i = 4; i < compressed_data.length; i++) {
 		// Jos törmättiin koodinvaihtomerkkiin, luetaan relatiivinen sijainti ja pituus
-		if (file.buffer[i] == 128) {
-			short location = file.buffer[i+1] | (file.buffer[i+2] << 8);
-			short length = file.buffer[i+3] | (file.buffer[i+4] << 8);
+		if (compressed_data.buffer[i] == 128) {
+			unsigned short location = parse_short(&compressed_data.buffer[i+1]);
+			unsigned short length = parse_short(&compressed_data.buffer[i+3]);
 			// Jos pituus on nolla, viittaus esittääkin literaalia koodinvaihtomerkkiä
 			if (length == 0) {
 				buffer[pointer++] = 128;
 			} else {
 				// Muulloin lisätään puskuriin etuliite aiempaa puskurista
-				long p = pointer;
+				unsigned long p = pointer;
 				for (long j = 0; j < length; j++) {
 					buffer[pointer++] = buffer[p - location + j];
 				}
@@ -33,12 +43,12 @@ void decompress(data file) {
 			i += 4;
 		} else {
 			// Muulloin lisätään puskuriin literaali pakatusta datasta
-			buffer[pointer++] = file.buffer[i];
+			buffer[pointer++] = compressed_data.buffer[i];
 		}
 	}
 
 	// Tulostetaan pakkaamaton data
-	for (long i = 0; i < pointer; i++) {
+	for (unsigned long i = 0; i < pointer; i++) {
 		putchar(buffer[i]);
 	}
 
